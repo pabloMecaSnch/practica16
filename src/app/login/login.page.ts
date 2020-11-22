ec34abf509f012b653d811a47ae5446b2f829e6c
Pablo <pablo.meca.sanchez@gmail.com>
2020-11-22 13:47:29 +0100

    final

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -34,55 +34,54 @@ export class LoginPage implements OnInit {
     public formBuilder: FormBuilder) {
       
      }
-
+     mayorEdad(e : any){
+      const fecha = new Date(e);
+      const timeDiff = Math.abs(Date.now()-fecha.getTime()) ;
+      const edad= Math.floor((timeDiff / (1000 * 3600 * 24))/365);
+      
+      if(edad>18){
+        //this.mayor=true;
+        return true;
+      }else{
+        return false;
+      }
+      
+    }
+    dni_validation(fg:FormGroup){
+      if(this.mayorEdad(fg.controls['age'].value)){
+        if(fg.controls['dni'].value==''){
+          return({dni_validation:true});
+        }else{
+          return(null);
+        }
+      }else{
+        return(null);
+      }
+    }
   ngOnInit() {
     this.dni_validation_group = new FormGroup({
       age : new FormControl('',Validators.compose([
-        Validators.required,
-        this.mayorEdad
-      ]
-      ))
-    }),
+        Validators.required
+      ])),
+      dni : new FormControl('',Validators.compose([
+        Validators.maxLength(9),
+        Validators.pattern('^([0-9]{8,8}[A-Za-z])?$')
+      ])),
+    },(formGroup : FormGroup) =>{
+      return this.dni_validation(formGroup);
+    });
+
     this.validations_form = new FormGroup({
       dni_validation : this.dni_validation_group,
       nombre : new FormControl('',Validators.compose([
         Validators.required
       ])),
-      dni : new FormControl('',Validators.compose([
-        Validators.required
-      ]))
-      
     });
    
   }
-  mayor(fc: FormControl){
-    if(this.age==true){
-      
-    }else{
-      return(null);
-    }
-    return(null);
-  }
-  mayorEdad(fc: FormControl){
-    const fecha = new Date(fc.value);
-    const timeDiff = Math.abs(Date.now()-fecha.getTime()) ;
-    const edad= Math.floor((timeDiff / (1000 * 3600 * 24))/365);
-    
-    if(edad>18){
-      //this.mayor=true;
-      alert(edad);
-      this.control();
-    }else{
-      //this.age=false;
-      //this.mayor=false;
-    }
-    return(null);
-  }
+ 
+  
 
-  method ;control(){
-    this.validations_form.get('dni').setValidators([Validators.required]);
-    this.validations_form.get('dni').updateValueAndValidity();
-  }
 }
 
   
